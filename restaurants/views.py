1c041e15560977529e43897a648162446bdee85d
riri-jp <alahmary.mashari@gmail.com>
2019-02-10 13:29:28 +0300

    Maram - Mashari

--- restaurants/views.py
+++ restaurants/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def first_view (request):
+    context = {
+        'msg' : 'Hello World!'
+    }
+    return render(request, 'test.html', context)
